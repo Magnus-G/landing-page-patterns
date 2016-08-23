46ea54f2bd87cad36c04c4b8e90914208b940e40
Magnus Jobbdator <magnus@Magnuss-MacBook-Pro.local>
2016-08-23 15:43:09 +0200

    Add a lot

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,3 @@
-// This is where it all goes :)
+$(document).ready(function() {
+    $('#example1').erToc({'goTopNode':'#example1', 'startLevel': 'h1', 'maxLevel': 'h1', 'numbered': false, 'numberedSuffix':'. '});
+});
